723688e48173957e855e81c275c957c7879d320e
keikeka <keilirosalesr@gmail.com>
2022-12-07 18:13:41 +0000

    Updated home component

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Jumbotrom from "./jumbotron.jsx";
 
 const Home = () => {
-	return (
-		<div className="text-center">
-			<Jumbotrom />			
-		</div>
-	);
+  return (
+    <div className="text-center">
+      <Jumbotrom />
+    </div>
+  );
 };
 
 export default Home;
