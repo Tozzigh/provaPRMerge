326263377b31e3ece5f885c9d708052571696331
keikeka <keilirosalesr@gmail.com>
2022-12-07 18:09:04 +0000

    Created jumbotron component

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Jumbotrom = () => {
+    return (        
+        <div className="container-fluid mt-5 text-center">
+            <h1 className="display-5 fw-bold">Custom jumbotron</h1>
+            <p className="col-md-12 fs-5">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+            <button className="btn btn-primary btn-lg" type="button">Example button</button>
+      </div>
+    );
+};
+
+export default Jumbotrom;
