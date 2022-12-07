99230b135f9e23630c78a8bfe91ffc8507c742cc
Tozzigh <tozzigh@gmail.com>
2022-12-07 18:15:11 +0000

    Added footer

---
+++ src/js/component/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer className="footer sticky-bottom py-3 bg-dark">
+            <div className="container">
+                <span className="text-muted">Place sticky footer content here.</span>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
